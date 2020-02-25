69698a08b689de5e644ecb4fe6e92a9e37a583c8
michaelwalen1 <41088736+michaelwalen1@users.noreply.github.com>
2020-02-25 15:46:21 -0500

    Add files via upload

---
+++ testPHPConnection.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+
+	</head>
+	<body>
+		
+		<?php
+		
+			// SQL Server Extension Sample Code:
+			$connectionInfo = array("UID" => "testPolyAR", "pwd" => "{your_password_here}", "Database" => "testAzureSQL", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+			$serverName = "tcp:testtestserver.database.windows.net,1433";
+			$connection = sqlsrv_connect($serverName, $connectionInfo);
+			
+			//$locationID = $_POST["location"];
+			//$tourType = $_POST["tourType"];
+			$locationID = 1002;
+			$tourType = 0;
+			
+			
+			if ($connection === false) {
+				die("Connection Failed: " . sqlsrv_errors());
+			}
+			
+			$sql = "SELECT thing FROM testAssets WHERE location=" . $locationID . " AND tourType=" . $tourType;
+
+			$statement = sqlsrv_query($connection, $sql);
+			
+			if (sqlsrv_fetch($statement) === false) {
+				die("Invalid Query: " . sqlsrv_errors());
+			}
+			
+			$fileName = sqlsrv_get_field($statement, 0);
+			
+			echo $fileName;
+			
+		?>
+			
+	</body>
+</html>
